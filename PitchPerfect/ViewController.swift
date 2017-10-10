88485ac4f0baafb40f2689089884380d0369f685
Felix D. Canaparro <felixcanaparro@gmail.com>
2017-10-09 21:14:16 -0300

    override viewDidAppear and viewWillAppear on viewcontroler.swift

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
