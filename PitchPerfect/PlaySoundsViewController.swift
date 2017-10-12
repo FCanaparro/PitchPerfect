20281c98e0ec4ac645db8f8c7892403bd36ab13f
Felix D. Canaparro <felixcanaparro@gmail.com>
2017-10-12 16:31:09 -0300

    Outlets for PlaySoundsVC created and linked to the buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,6 +11,15 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     var recorderdAudioUrl : URL!
+    @IBOutlet weak var snailButton : UIButton!
+    @IBOutlet weak var chipmunkButton : UIButton!
+    @IBOutlet weak var rabbitButton : UIButton!
+    @IBOutlet weak var vaderButton : UIButton!
+    @IBOutlet weak var echoButton : UIButton!
+    @IBOutlet weak var reverbButton : UIButton!
+    @IBOutlet weak var stopButton : UIButton!
+    
+    
 
     
     
